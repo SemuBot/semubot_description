60035c6d7e6c52607515067011d9ed87f27e5322
Anton Laidmets <antonlaidmets@gmail.com>
2026-03-04 13:51:45 +0200

    Added Semubot description

---
+++ launch/display_robot.launch.py
@@ -0,0 +1,68 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command
+from ament_index_python.packages import get_package_share_path
+import os
+
+def generate_launch_description():
+    pkg = get_package_share_path("semubot_description")
+    
+    urdf_path = os.path.join(
+        pkg,
+        'urdf',
+        'semubot.urdf.xacro')
+    
+    rviz_path = os.path.join(
+        pkg,
+        'config',
+        'semubot_descrption.rviz')
+
+    robot_description_content = Command(['xacro ', urdf_path])
+
+    # Robot State Publisher
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[
+            {'robot_description': robot_description_content}
+        ]
+    )
+
+    # Joint State Publisher
+    joint_state_publisher = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        output='screen',
+        parameters=[
+            {'robot_description': robot_description_content}
+        ]
+    )
+
+    #Static transform
+    static_tf = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='static_transform_publisher',
+        arguments=['--frame-id', 'odom', '--child-frame-id', 'dummy_root']
+    )
+
+    # RViz2
+    rviz2 = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=[
+            '-d', rviz_path
+        ]
+    )
+
+    return LaunchDescription([
+        robot_state_publisher,
+        joint_state_publisher,
+        static_tf,
+        rviz2
+    ])
